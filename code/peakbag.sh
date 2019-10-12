4714c5e5bb01554d39dccbbe802ac41eae9b6214
Oliver Hall <ojh251@bham.ac.uk>
2019-10-12 17:58:16 +0100

    Updated ready for a validation run

--- code/peakbag.sh
+++ code/peakbag.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Run our model on all stars
-for idx in {0..94}; do
+#for idx in {0..94}; do
+#    sbatch --export=idx=$idx peakbag.bear
+#done
+for idx in  0, 2, 3, 4, 9, 12, 17, 18, 24, 29, 30, 34, 43, 44, 50, 52, 54, 56, 66, 68, 69, 73, 75, 86, 92; do
     sbatch --export=idx=$idx peakbag.bear
 done
+    
