39907b34ee0751989e00586a98df116c03e24fc0
Oliver Hall <ojh251@bham.ac.uk>
2019-12-06 16:10:41 +0000

    Bash updates

--- code/peakbag.sh
+++ code/peakbag.sh
@@ -2,7 +2,7 @@
 
 # Run our model on all stars
 
-for idx in  1 3 10 21 23 27 28 29 30 32 36 47 55 57 61 62 63 67 74 76 80 81 83 84 88 89 94; do
+for idx in  1 23; do
     # echo $idx
     sbatch --export=idx=$idx peakbag.bear
 done
